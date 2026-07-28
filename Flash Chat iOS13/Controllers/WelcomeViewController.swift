cad32c867f21927fdb2fb4070694288e32f5fb9d
JonasMahlburg <darrylmanga@yahoo.com>
2026-07-28 10:06:53 +0200

    refactoring Strings - add Constants.swift with needed Strings - replacing Strings with constants from Constant.swift

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -16,7 +16,7 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = "⚡️Flashchat"
+        titleLabel.text = K.appName
        
     }
     
