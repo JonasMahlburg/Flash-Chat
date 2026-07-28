b344950de246f7f8791d18494ee7b699a98a122d
JonasMahlburg <darrylmanga@yahoo.com>
2026-07-28 11:35:54 +0200

    feat: MessageBubbles - added Message Struct - added custom MessageCell - added example Messages for tesing

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,10 +14,20 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "obiwan@kenobi.com", body: "Hello there"),
+        Message(sender: "anakin@skywalker", body: "You underestimate my Power"),
+        Message(sender: "obiwan@kenobi.com", body: "Don't try it.")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
         
     }
     
@@ -35,4 +45,16 @@ class ChatViewController: UIViewController {
         }
     }
     
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        return cell
+    }
 }
---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Jonas Mahlburg on 28.07.26.
+//  Copyright © 2026 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // emailadress of sender exp: test@test.com
+    let body: String  //actual Message
+}
